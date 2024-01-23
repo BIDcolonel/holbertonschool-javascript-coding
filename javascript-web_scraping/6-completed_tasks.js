04953d7193ca167a8b18b48c6aeff475c0aa1ab5
BIDcolonel <ptitofdu89@live.fr>
2024-01-23 19:21:46 +0100

    comment and const added

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-request(process.argv[2], (error, response, body) => {
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const todo = JSON.parse(body);
+
     const completedTasksByUser = {};
+
+    // Browses tasks in the JSON response.
     for (const task of todo) {
+      // Checks if the task has been completed.
       if (task.completed) {
+        // Gets the user ID of the task.
         const userId = task.userId;
+
         if (completedTasksByUser[userId]) {
           completedTasksByUser[userId]++;
         } else {
+        // user has completed its 1st task.
           completedTasksByUser[userId] = 1;
         }
       }

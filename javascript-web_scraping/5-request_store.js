60f659c89ef59447d1f516bfcfff45209ed38124
BIDcolonel <ptitofdu89@live.fr>
2024-01-23 11:31:28 +0100

    semistandard correction

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,13 +4,13 @@ const request = require('request');
 const fs = require('fs');
 
 request(process.argv[2], (error, response, body) => {
-    if (error) {
-        console.error(`${error}`);
-    } else {
-        fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
-            if (err) {
-                console.error(err);
-            }
-        });
-    }
+  if (error) {
+    console.error(`${error}`);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 });

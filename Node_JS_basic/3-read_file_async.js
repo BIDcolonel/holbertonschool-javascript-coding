1e582feaa6c690ae56507b98bfc14a3312394272
BIDcolonel <ptitofdu89@live.fr>
2024-01-30 11:59:11 +0100

    Correction code + fix linter

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,31 +1,27 @@
 const fs = require('fs');
 
-function countStudents(path) {
+async function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
+    fs.readFile(path, 'utf-8', (error, data) => {
       if (error) {
         reject(new Error('Cannot load the database'));
-      } else {
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const fields = {};
-        let totalStudents = 0;
-
-        for (let i = 1; i < lines.length; i++) {
-          const [firstName, lastName, age, field] = lines[i].split(',');
-          if (!fields[field]) {
-            fields[field] = [];
-          }
-          fields[field].push(firstName);
-          totalStudents++;
-        }
-
-        console.log(`Number of students: ${totalStudents}`);
-        for (const field in fields) {
-          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-        }
-
-        resolve();
+        return;
       }
+
+      const students = data.split('\n').filter(Boolean).slice(1).map((line) => {
+        const [firstname, lastname, age, field] = line.split(',');
+        return {
+          firstname, lastname, age, field,
+        };
+      });
+
+      const csStudents = students.filter((student) => student.field === 'CS');
+      const sweStudents = students.filter((student) => student.field === 'SWE');
+
+      console.log(`Number of students: ${students.length}`);
+      console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map((student) => student.firstname).join(', ')}`);
+      console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map((student) => student.firstname).join(', ')}`);
+      resolve();
     });
   });
 }

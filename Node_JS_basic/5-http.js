1e4d8ab013e35fef60ed67ee8601140aa2fd3555
BIDcolonel <ptitofdu89@live.fr>
2024-01-31 23:37:03 +0100

    correction code

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,39 +1,22 @@
 const http = require('http');
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const port = 1245;
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    const database = 'database.csv';
-    fs.readFile(database, 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      } else {
-        const students = data.trim().split('\n').slice(1); // Exclude the header
-        const totalStudents = students.length;
-
-        const csStudents = students.filter(student => student.split(',')[3].trim() === 'CS');
-        const sweStudents = students.filter(student => student.split(',')[3].trim() === 'SWE');
-
-        const totalCsStudents = csStudents.length;
-        const totalSweStudents = sweStudents.length;
-
-        const csStudentsList = csStudents.map(student => student.split(',')[0].trim()).join(', ');
-        const sweStudentsList = sweStudents.map(student => student.split(',')[0].trim()).join(', ');
-
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(`This is the list of our students\nNumber of students: ${totalStudents}\nNumber of students in CS: ${totalCsStudents}. List: ${csStudentsList}\nNumber of students in SWE: ${totalSweStudents}. List: ${sweStudentsList}`);
-      }
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
+    res.write('Hello Holberton School!');
   }
-});
-
-app.listen(1245);
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.end();
+}).listen(port);
 
 module.exports = app;

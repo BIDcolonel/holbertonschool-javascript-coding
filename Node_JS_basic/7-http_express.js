851e16db80c55b04d18edf48c66700e9c2e51623
BIDcolonel <ptitofdu89@live.fr>
2024-02-01 10:17:07 +0100

    back to base code

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,9 +1,5 @@
 const express = require('express');
-
-const args = process.argv.slice(2);
-const countStudents = require('./3-read_file_async');
-
-const DATABASE = args[0];
+const fs = require('fs');
 
 const app = express();
 const port = 1245;
@@ -12,18 +8,29 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  const msg = 'This is the list of our students\n';
-  try {
-    const students = await countStudents(DATABASE);
-    res.send(`${msg}${students.join('\n')}`);
-  } catch (error) {
-    res.send(`${msg}${error.message}`);
-  }
+app.get('/students', (req, res) => {
+  const database = process.argv[2];
+  let content = 'This is the list of our students\n';
+
+  fs.readFile(database, 'utf-8', (err, data) => {
+    if (err) {
+      content += 'Cannot load the database\n';
+    } else {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const csStudents = lines.filter((line, index) => index > 0 && line.endsWith('CS')).map((line) => line.split(',')[0]);
+      const sweStudents = lines.filter((line, index) => index > 0 && line.endsWith('SWE')).map((line) => line.split(',')[0]);
+
+      content += `Number of students: ${lines.length - 1}\n`;
+      content += `Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`;
+      content += `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`;
+    }
+
+    res.send(content);
+  });
 });
 
 app.listen(port, () => {
-  // console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server listening on port ${port}`);
 });
 
 module.exports = app;

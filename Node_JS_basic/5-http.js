15992b1d0d405bf9f7bf6b588b94088aebbe9fdb
BIDcolonel <ptitofdu89@live.fr>
2024-02-02 09:55:46 +0100

    Back other version + fix linter

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,52 +1,55 @@
 const http = require('http');
 const fs = require('fs');
 
-async function countStudents(filepath) {
-  try {
-    const csv = await fs.promises.readFile(filepath, { encoding: 'utf8' });
-    const headerArray = csv.split(/\r?\n|\n/);
-    const headers = headerArray[0].split(',');
+function countStudents(filepath) {
+  return new Promise((resolve, res) => {
+    fs.promises.readFile(filepath, { encoding: 'utf8' })
+      .then((csv) => {
+        const headerArray = csv.split(/\r?\n|\n/);
+        const headers = headerArray[0].split(',');
 
-    const dictList = [];
-    const noHeaderArray = headerArray.slice(1);
-    for (let i = 0; i < noHeaderArray.length; i += 1) {
-      const data = noHeaderArray[i].split(',');
-      if (data.length === headers.length) {
-        const row = {};
-        for (let j = 0; j < headers.length; j += 1) {
-          row[headers[j].trim()] = data[j].trim();
+        const dictList = [];
+        const noHeaderArray = headerArray.slice(1);
+        for (let i = 0; i < noHeaderArray.length; i += 1) {
+          const data = noHeaderArray[i].split(',');
+          if (data.length === headers.length) {
+            const row = {};
+            for (let j = 0; j < headers.length; j += 1) {
+              row[headers[j].trim()] = data[j].trim();
+            }
+            dictList.push(row);
+          }
         }
-        dictList.push(row);
-      }
-    }
 
-    let countCS = 0;
-    let countSWE = 0;
-    const studentsCS = [];
-    const studentsSWE = [];
+        let countCS = 0;
+        let countSWE = 0;
+        const studentsCS = [];
+        const studentsSWE = [];
 
-    dictList.forEach((element) => {
-      if (element.field === 'CS') {
-        countCS += 1;
-        studentsCS.push(element.firstname);
-      } else if (element.field === 'SWE') {
-        countSWE += 1;
-        studentsSWE.push(element.firstname);
-      }
-    });
+        dictList.forEach((element) => {
+          if (element.field === 'CS') {
+            countCS += 1;
+            studentsCS.push(element.firstname);
+          } else if (element.field === 'SWE') {
+            countSWE += 1;
+            studentsSWE.push(element.firstname);
+          }
+        });
 
-    const countStudents = countCS + countSWE;
+        const countStudents = countCS + countSWE;
 
-    return ({
-      countStudents,
-      countCS,
-      countSWE,
-      studentsCS,
-      studentsSWE,
-    });
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+        resolve({
+          countStudents,
+          countCS,
+          countSWE,
+          studentsCS,
+          studentsSWE,
+        });
+      })
+      .catch(() => {
+        res.end('Error: Cannot load the database');
+      });
+  });
 }
 
 const pathToDB = process.argv[2];
@@ -75,7 +78,6 @@ const app = http.createServer((req, res) => {
       })
       .catch(() => {
         res.end('Error: Cannot load the database');
-        throw new Error('Cannot load the database');
       });
   }
 });
